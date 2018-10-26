5b81e88c0ad9979e6c13d1b7958d842322d6ea96
Paul Norberger <paul.norberger@s2018.tu-chemnitz.de>
2018-10-26 13:55:15 +0200

    Third exercise completed

--- Uebungen/10_power/power.c
+++ Uebungen/10_power/power.c
@@ -1,9 +1,27 @@
 int power(int x, unsigned int n) {
-    if (n==0) return 1;
+    if (n == 0) return 1;
 
     int sum = x;
-    for (int i=2; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
         sum *= x;
+    }
+
+    return sum;
+}
+
+int poly1(int x, unsigned int n) {
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+        sum += power(x, i);
+    }
+    
+    return sum;
+}
+
+int polyAlter(int x, unsigned int n) {
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+        sum += power(-1, i) * power(x, i);
     }
 
     return sum;
--- Uebungen/10_power/powertest.c
+++ Uebungen/10_power/powertest.c
@@ -2,9 +2,15 @@
 
 // Declaration
 unsigned int power(int, unsigned int);
+unsigned int poly1(int, unsigned int);
+unsigned int polyAlter(int, unsigned int);
 
 int main(int argc, char const *argv[])
 {
     printf("power(%d, %d) = %d\n", 3, 4, power(3, 4));
+    printf("poly1(%d, %d) = %d\n", 2, 3, poly1(2, 3));
+    printf("manual poly1(2,3): power(2,0) + power(2,1) + power(2,2) + power(2,3) = %d\n", power(2,0) + power(2,1) + power(2,2) + power(2,3));
+    printf("polyAlter(%d, %d) = %d\n", 2, 3, polyAlter(2, 3));
+    printf("manual polyAlter(2,3): power(2,0) - power(2,1) + power(2,2) - power(2,3) = %d\n", power(2,0) - power(2,1) + power(2,2) - power(2,3));
     return 0;
 }
